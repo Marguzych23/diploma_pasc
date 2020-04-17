003cac1b5bf0283fe673f3e51bd5ca2fda6c4fc5
Emil Zaynullin <zainullin.e@mxm.email>
2020-04-17 05:41:58 +0300

    update

--- src/Entity/EmailSubscriber.php
+++ src/Entity/EmailSubscriber.php
@@ -3,6 +3,7 @@
 
 namespace App\Entity;
 
+use DateTime;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -28,4 +29,73 @@ class EmailSubscriber
      * @ORM\JoinTable(name="email_subscribers_industries")
      */
     private Collection $industries;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private ?DateTime $lastSubscribeDate = null;
+
+    /**
+     * @return int
+     */
+    public function getId() : int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id) : void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail() : string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail(string $email) : void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return Collection
+     */
+    public function getIndustries() : Collection
+    {
+        return $this->industries;
+    }
+
+    /**
+     * @param Collection $industries
+     */
+    public function setIndustries(Collection $industries) : void
+    {
+        $this->industries = $industries;
+    }
+
+    /**
+     * @return DateTime|null
+     */
+    public function getLastSubscribeDate() : ?DateTime
+    {
+        return $this->lastSubscribeDate;
+    }
+
+    /**
+     * @param DateTime|null $lastSubscribeDate
+     */
+    public function setLastSubscribeDate(?DateTime $lastSubscribeDate) : void
+    {
+        $this->lastSubscribeDate = $lastSubscribeDate;
+    }
 }

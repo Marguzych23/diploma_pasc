e2dc3830e1ab06b88a84c1186da826edacc6169f
Emil Zaynullin <zainullin.e@mxm.email>
2020-05-29 02:35:56 +0300

    notify

---
+++ src/Migrations/Version20200528233456.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200528233456 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE email_subscriber ADD email_notify TINYINT(1) NOT NULL, ADD last_email_notify_date DATETIME DEFAULT NULL, CHANGE last_subscribe_date last_notify_date DATETIME DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE email_subscriber ADD last_subscribe_date DATETIME DEFAULT NULL, DROP last_notify_date, DROP email_notify, DROP last_email_notify_date');
+    }
+}
